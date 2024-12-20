9007e9bfaefdff3a3cc592e5f6332b1d4edbb601
Muhammad Firdous <firdous.akhss19@gmail.com>
2024-12-20 11:12:39 +0600

    sddd

---
+++ src/main/java/org/example/subscriptionmanagement/SubscriptionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.subscriptionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionManagementSystemApplication.class, args);
+    }
+
+}
